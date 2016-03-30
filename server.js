4ca3d402ae039ed05eaa7e29f408919e8ebb2a3a
Victor del Valle <vicalle49@gmail.com>
2016-03-30 20:29:08 +0200

    Revert "fix error handling + PUT"
    This reverts commit 5983f34f8d7bdcf2fe5d6aa16e87afe5bdf05161.

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use('/evento',  eventoRouter);
 // CONTACTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
+function handleError(reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
@@ -62,7 +62,7 @@ function handleError(res, reason, message, code) {
 app.get("/contacts", function(req, res) {
   db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
     if (err) {
-      handleError(res, err.message, "Failed to get contacts.");
+      handleError(err.message, "Failed to get contacts.");
     } else {
       res.status(200).json(docs);  
     }
@@ -74,12 +74,12 @@ app.post("/contacts", function(req, res) {
   newContact.createDate = new Date();
 
   if (!(req.body.firstName || req.body.lastName)) {
-    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+    handleError("Invalid user input", "Must provide a first or last name.", 400);
   }
 
   db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
+      handleError(err.message, "Failed to create new contact.");
     } else {
       res.status(201).json(doc.ops[0]);
     }
@@ -95,7 +95,7 @@ app.post("/contacts", function(req, res) {
 app.get("/contacts/:id", function(req, res) {
   db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to get contact");
+      handleError(err.message, "Failed to get contact");
     } else {
       res.status(200).json(doc);  
     }
@@ -104,11 +104,10 @@ app.get("/contacts/:id", function(req, res) {
 
 app.put("/contacts/:id", function(req, res) {
   var updateDoc = req.body;
-  delete updateDoc._id;
 
   db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to update contact");
+      handleError(err.message, "Failed to update contact");
     } else {
       res.status(204).end();
     }
@@ -118,7 +117,7 @@ app.put("/contacts/:id", function(req, res) {
 app.delete("/contacts/:id", function(req, res) {
   db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
     if (err) {
-      handleError(res, err.message, "Failed to delete contact");
+      handleError(err.message, "Failed to delete contact");
     } else {
       res.status(204).end();
     }
